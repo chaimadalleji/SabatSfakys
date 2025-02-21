2ff6665302e81fa3ec6309722f26c3a1caadcd6e
chaima dalleji <chaimadalleji1@gmail.com>
2025-02-21 15:23:08 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SabatSfakys',
+  webDir: 'www'
+};
+
+export default config;
